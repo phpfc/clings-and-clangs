d6a8e104002f956a9fff17c4ff592f355c22cbf7
PedroFranca2299 <pedrofranca-tech@proton.me>
2025-10-03 16:37:30 -0300

    get smallest prime number in matrix

---
+++ pointers/get_matrix_min_prime.c
@@ -0,0 +1,75 @@
+/* 
+  6) Faça uma função que receba como parâmetro uma matriz M[15][15] 
+  e retorne: o menor número primo da matriz e a sua respectiva posição 
+  (linha e coluna). O protótipo da função é dado por:
+
+  Protótipo da função:
+
+  void menor_primo(int M[15][15], int *primo, int *lin, int *col)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <stdbool.h>
+#define N 15
+
+
+bool is_prime(int num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (int i = 2; i * i <= num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void get_matrix_min_prime(int m[N][N], int *min_prime, int *row, int *col) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (*min_prime > m[i][j] && is_prime(m[i][j])) {
+        *min_prime = m[i][j];
+        *row = i;
+        *col = j;
+      }
+    }
+  }
+}
+
+void create_matrix(int m[N][N]) {
+  int ground = 12;
+  srand(time(NULL));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      m[i][j] = (rand() % (100 - ground)) + ground;
+    }
+  }
+}
+
+void print_matrix(int m[N][N]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      printf("% 3d", m[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int main() {
+  int matrix[N][N];
+  int min_prime;
+  int idx_row = 0;
+  int idx_col = 0;
+
+  create_matrix(matrix);
+  print_matrix(matrix);
+  get_matrix_min_prime(matrix, &min_prime, &idx_row, &idx_col);
+
+  printf("The smallest prime value is %d at row %d, column %d\n", min_prime, idx_row, idx_col);
+}
